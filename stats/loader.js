8108efd866247d8461d4f6a90f0437e07694dbea
Mason Williams <154944752+baconatorr@users.noreply.github.com>
2024-08-03 01:03:28 -0400

    Add files via upload

---
+++ stats/loader.js
@@ -0,0 +1,6 @@
+setTimeout(function() {
+  window.location.href= 'stats.html';
+  }, 1300)
+
+  var audio = document.getElementById('menu');
+  audio.play();
---
+++ stats/stats.js
@@ -0,0 +1,25 @@
+loadStats();
+
+function loadStats(){
+  let matches = parseInt(localStorage.getItem('matches')) || 0;
+  document.querySelector('#matchesStat').innerText = matches.toLocaleString();
+
+  let wins = parseInt(localStorage.getItem('wins')) || 0;
+  document.querySelector('#winsStat').innerText = wins.toLocaleString();
+
+  let wordsPlaced = parseInt(localStorage.getItem('total words')) || 0;
+  document.querySelector('#wordsPlacedStat').innerText = wordsPlaced.toLocaleString();
+
+  let lettersPlaced = parseInt(localStorage.getItem('total letters')) || 0;
+  document.querySelector('#lettersPlacedStat').innerText = lettersPlaced.toLocaleString();
+
+  let skipsUsed = parseInt(localStorage.getItem('skipsUsed')) || 0;
+  document.querySelector('#skipsUsedStat').innerText = skipsUsed.toLocaleString();
+
+  let skiplessWins = parseInt(localStorage.getItem('skiplessWins')) || 0;
+  document.querySelector('#skiplessWinsStat').innerText = skiplessWins.toLocaleString();
+}
+
+function home(){
+  document.location.href = '/home/home.html';
+}
